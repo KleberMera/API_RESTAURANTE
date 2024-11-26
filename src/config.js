01c4fa857dc4f69589dd4c5719d53b3e65dd7f5e
KleberMera <klebermera2016@gmail.com>
2024-11-26 13:21:49 -0500

    config variables

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 import { config } from "dotenv";
 config();
 
-export const BD_HOST = process.env.BD_HOST || "127.0.0.1";
-export const BD_DATABASE = process.env.BD_DATABASE || "restaurante";
+export const BD_HOST = process.env.BD_HOST || "autorack.proxy.rlwy.net";
+export const BD_DATABASE = process.env.BD_DATABASE || "railway";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "";
 export const DB_PORT = process.env.DB_PORT || 3306;
